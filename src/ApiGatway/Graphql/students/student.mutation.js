ac89d5409513b72ab983866905337cc75a86e0cb
junaid102004 <junaidkhan610049.com>
2026-02-14 22:28:14 +0530

    new bugs fixed

--- src/ApiGatway/Graphql/students/student.mutation.js
+++ src/ApiGatway/Graphql/students/student.mutation.js
@@ -1,36 +1,36 @@
 import { gql } from "apollo-server-express";
 
 export const studentMutation = gql`
-   extend type Mutation {
-        createStudent(
-            firstName: String!
-            lastName: String!
-            gender: String!
-            dateOfBirth: String!
-            mobileNumber: String!
-            address: String!
-            class: String!
-            section: String!
-            rollNumber: String!
-            admissionDate: String!
-            status: String!
-        ): StudentCreateResponse!
+  extend type Mutation {
+    createStudent(
+      firstName: String!
+      lastName: String!
+      gender: String!
+      dateOfBirth: String!
+      mobileNumber: String!
+      address: String!
+      className: String!
+      section: String!
+      rollNumber: String!
+      admissionDate: String!
+      status: String!
+    ): StudentCreateResponse!
 
-        updateStudent(
-            studentId: Int!
-            firstName: String!
-            lastName: String!
-            gender: String!
-            dateOfBirth: String!
-            mobileNumber: String!
-            address: String!
-            class: String!
-            section: String!
-            rollNumber: String!
-            admissionDate: String!
-            status: String!
-        ): StudentCreateResponse!
+    updateStudent(
+      studentId: Int!
+      firstName: String!
+      lastName: String!
+      gender: String!
+      dateOfBirth: String!
+      mobileNumber: String!
+      address: String!
+      className: String!
+      section: String!
+      rollNumber: String!
+      admissionDate: String!
+      status: String!
+    ): StudentCreateResponse!
 
-        deleteStudent(studentId: Int!): Boolean!
+    deleteStudent(studentId: Int!): Boolean!
   }
-`
+`;
